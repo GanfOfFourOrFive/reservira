945e4f689c6d3f6bdd062fa717527dd374d39cfe
Marcelo J. Both <marcelo.both@gmail.com>
2014-06-08 16:33:49 -0300

    corrections for hound complaints

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,7 +5,7 @@ FactoryGirl.define do
     password "please123"
   end
 
-  factory :confirmed_user, :parent => :user do
+  factory :confirmed_user, parent: :user do
     after(:create) { |user| user.confirm! }
   end
 end
