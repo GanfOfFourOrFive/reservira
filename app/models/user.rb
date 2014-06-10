74b1058e303b96ec911fbbf2b6655c25432a409f
Roberto Schneiders <roberto.schneiders@gmail.com>
2014-06-09 22:02:10 -0300

    RouncCI requests. Round one

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,9 +11,11 @@ class User < ActiveRecord::Base
     self.role ||= :user
   end
 
-  def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
+  def self.find_for_google_oauth2(access_token, signed_in_resource = nil)
     data = access_token.info
-    user = User.where(:provider => access_token.provider, :uid => access_token.uid ).first
+    provider = access_token.provider
+    uid = access_token.uid
+    user = User.where(provider: provider, uid: uid ).first
     if user
       return user
     else
@@ -21,9 +23,9 @@ class User < ActiveRecord::Base
       if registered_user
         return registered_user
       else
-        user = User.create(name: data["name"],
+        user = User.create(name: data['name'],
           provider:access_token.provider,
-          email: data["email"],
+          email: data['email'],
           uid: access_token.uid ,
           password: Devise.friendly_token[0,20],
         )
