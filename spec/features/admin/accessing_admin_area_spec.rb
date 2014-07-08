0ef390731c153acd84814156c397e61af0dd4328
Marcelo J. Both <marcelo.both@gmail.com>
2014-07-07 23:04:06 -0300

    Only autenticated user can access

--- spec/features/admin/accessing_admin_area_spec.rb
+++ spec/features/admin/accessing_admin_area_spec.rb
@@ -2,6 +2,11 @@ require "rails_helper" # use rails_helper caso esteja usando rails composer
 
 feature "Acesando a area administrativa" do
   scenario "posso acessar como administrador" do
+    user = FactoryGirl.create(:confirmed_user)
+    signin(user.email, user.password)
+
     visit admin_root_path
+
+    expect(page).to have_content("Área administrativa")
   end
 end
