d5eb8fb2c32bafbcb21b9940e6518510f7c996ae
Vande Sehn <vande.sehn@gmail.com>
2014-07-28 23:28:02 +0000

    ajustes

--- spec/features/locales/creating_locales_spec.rb
+++ spec/features/locales/creating_locales_spec.rb
@@ -1,16 +1,19 @@
 require "rails_helper"
 
 feature "Criando Localidades" do
-  scenario "posso criar uma localidade" do
+  before do
     visit "/"
+    click_link "Nova Localidade"
+  end
 
+  scenario "posso criar uma localidade" do
     click_link "Nova Localidade"
 
     fill_in "Cidade", with: "Endereço 1"
     fill_in "Endereço", with: "Endereço 1 (um)"
     click_button "Criar localidade"
 
-    expect(page).to have_content("Localidade foi criada.")
+    expect(page).to have_content("Nome é muito curto (mínimo: 5 caracteres)")
   end
 
 end
