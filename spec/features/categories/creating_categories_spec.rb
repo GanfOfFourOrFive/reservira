005923db4c684134ed42105e25c5b08bbd91b115
Marcelo Melz <m.melz@hotmail.com>
2014-07-28 23:33:57 +0000

    Adicionado teste

--- spec/features/categories/creating_categories_spec.rb
+++ spec/features/categories/creating_categories_spec.rb
@@ -1,6 +1,11 @@
 require "rails_helper"
 
 feature "Criando Categorias" do
+  before do
+    visit '/'
+    click_link 'Nova Categoria'
+  end
+
   scenario "posso criar uma categoria" do
     visit "/"
 
@@ -10,5 +15,12 @@ feature "Criando Categorias" do
     click_button "Criar categoria"
 
     expect(page).to have_content("Categoria foi criada.")
+  end
+
+  scenario "com nome inválido não posso criar uma categoria" do
+    fill_in 'Nome', with: ''
+    click_button 'Criar categoria'
+
+    expect(page).to have_content('Nome é muito curto (mínimo: 5 caracteres)')
   end
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,33 @@
 require "rails_helper"
 
-RSpec.describe Category, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe Category do
+    before do
+      @category = Category.new(name: "Nova categoria.")
+    end
+
+    describe "quando o nome não foi informado" do
+      before { @category.name = ""}
+      it { should_not be_valid }
+    end
+
+    describe "quando o nome é muito curto" do
+      before { @category.name = "na"}
+      it { should_not be_valid }
+    end
+
+    describe "quando o nome é muito longo" do
+      before { @category.name = "n" * 50}
+      it { should_not be_valid }
+    end
+
+  describe "quando o nome de categoria já está sendo usado" do
+    before do
+      category_with_same_name = @category.dup
+      category_with_same_name.name = @category.name
+      category_with_same_name.save
+    end
+
+    it { should_not be_valid }
+  end
+
 end
