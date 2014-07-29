c4e768ae3d58a191d6da9d4525e4a50d40983e99
Marcelo Melz <m.melz@hotmail.com>
2014-07-29 01:05:29 +0000

    Codigo com bug

--- spec/features/categories/reading_categories_spec.rb
+++ spec/features/categories/reading_categories_spec.rb
@@ -1,9 +1,13 @@
    require "rails_helper"
 
   feature "Listando Categorias" do
-    scenario "listando todos os registros cadastrados" do
+    before do
       visit categories_path
-
       expect(page).to have_selector('h2', text: 'Cadastro de categorias')
+    end
+
+    scenario "listando todos os registros cadastrados" do
+      @category = FactoryGirl.create(:category)
+      expect(page).to have_content(@category.name)
     end
   end
