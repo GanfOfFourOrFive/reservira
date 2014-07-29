d4fb31ee1bb670e7d12e04e79d11febed952ff3a
Marcelo J. Both <marcelo.both@gmail.com>
2014-07-28 21:28:19 -0300

    Ajuste no erro do template erb

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,9 @@ ActiveRecord::Schema.define(version: 20140630235226) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "locales", force: true do |t|
     t.string   "city"
