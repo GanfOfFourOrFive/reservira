d5bbd24766e6279c46da7013b1c7d26325198954
Marcelo J. Both <marcelo.both@gmail.com>
2014-09-01 20:56:57 -0300

    Ajustes para o hound CI

--- spec/features/locales/reading_locales_spec.rb
+++ spec/features/locales/reading_locales_spec.rb
@@ -1,14 +1,9 @@
 require "rails_helper"
 
 feature "Listando Localidades" do
-
   before do
     visit locales_path
-    expect(page).to have_selector('h2', text: 'Cadastro de localidades')
+    expect(page).to have_selector("h2", text: "Cadastro de localidades")
     FactoryGirl.create(:locale)
-  end
-
-  scenario "listando todos os registros cadastrados" do
-    #expect(page).to have_content("MyString")
   end
 end
--- spec/models/locale_spec.rb
+++ spec/models/locale_spec.rb
@@ -1,33 +1,33 @@
 require "rails_helper"
 
-  describe Locale do
-    before do
-      @locale = Locale.new(city: "Cadastro Exemplo")
-    end
-
-    describe "quando o nome não foi informado" do
-      before { @locale.city = ""}
-      it { should_not be_valid }
-    end
-
-    describe "quando o nome é muito curto" do
-      before { @locale.city = "na"}
-      it { should_not be_valid }
-    end
-
-    describe "quando o nome é muito longo" do
-      before { @locale.city = "n" * 50}
-      it { should_not be_valid }
-    end
-
-    describe "quando o nome do local já está sendo usado" do
-      before do
-        locale_with_same_city = @locale.dup
-        locale_with_same_city.city = @locale.city
-        locale_with_same_city.save
-      end
-
-      it { should_not be_valid }
-    end
-
+describe Locale do
+  before do
+    @locale = Locale.new(city: "Cadastro Exemplo")
   end
+
+  describe "quando o nome não foi informado" do
+    before { @locale.city = "" }
+    it { should_not be_valid }
+  end
+
+  describe "quando o nome é muito curto" do
+    before { @locale.city = "na" }
+    it { should_not be_valid }
+  end
+
+  describe "quando o nome é muito longo" do
+    before { @locale.city = "n" * 50 }
+    it { should_not be_valid }
+  end
+
+  describe "quando o nome do local já está sendo usado" do
+    before do
+      locale_with_same_city = @locale.dup
+      locale_with_same_city.city = @locale.city
+      locale_with_same_city.save
+    end
+
+    it { should_not be_valid }
+  end
+
+end
