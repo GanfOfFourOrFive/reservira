e266cbf361edad9fec5a2e4d5bc1d9f92750bd99
Vande <vande.sehn@gmail.com>
2014-09-16 00:12:20 +0000

    Melhorias ao cadastro de localidades

--- app/controllers/locales_controller.rb
+++ app/controllers/locales_controller.rb
@@ -1,4 +1,5 @@
 class LocalesController < ApplicationController
+  before_action :find_locale, only: [:edit, :update, :destroy]
 
   def index
     @locales = Locale.all
@@ -12,20 +13,37 @@ class LocalesController < ApplicationController
     @locale = Locale.new(locale_params)
 
     if @locale.save
-      redirect_to @locale, notice: "Localidade foi criada."
+      redirect_to locales_path, notice: "Localidade foi criada."
     else
       flash[:alert] = "Localidade não pode ser criada."
       render "new"
     end
   end
 
-  def show
-    @locale = Locale.find(params[:id])
+  def edit
+
+  end
+
+  def update
+    if @locale.update(locale_params)
+        redirect_to locales_path, notice: 'Alterado com sucesso.'
+     else
+       render :edit
+     end
+  end
+
+  def destroy
+    @locale.destroy
+    redirect_to locales_url, notice: 'Feiiitooooo.'
   end
 
   private
 
   def locale_params
     params.require(:locale).permit(:city, :address)
+  end
+
+  def find_locale
+    @locale = Locale.find(params[:id])
   end
 end
